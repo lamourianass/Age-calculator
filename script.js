5c87d57cf782661bf9d91c0aeee9363c6e1dd5ca
Anass Lamouri <anasslamouri@icloud.com>
2025-08-11 14:22:06 +0100

    console log birthday value to test

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const birthdayEl = document.getElementById('birthday')
 
 function calculateAge() {
   const birthdayValue = birthdayEl.value
-  
+  console.log(birthdayValue)
 }
 
 btnEl.addEventListener('click', calculateAge)
