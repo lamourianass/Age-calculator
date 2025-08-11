089d5846644a2193acf0a975ea3656c4dc631f3d
Anass Lamouri <anasslamouri@icloud.com>
2025-08-11 14:21:31 +0100

    create const birthdayValue

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const btnEl = document.getElementById('btn')
 const birthdayEl = document.getElementById('birthday')
 
 function calculateAge() {
-  console.log('Your are dead haha')
+  const birthdayValue = birthdayEl.value
+  
 }
 
 btnEl.addEventListener('click', calculateAge)
