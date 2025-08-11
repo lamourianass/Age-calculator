9464b4c1b94425023e299b322f15f946ad6cb965
Anass Lamouri <anasslamouri@icloud.com>
2025-08-11 14:25:08 +0100

    Alert if input is empty

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const birthdayEl = document.getElementById('birthday')
 
 function calculateAge() {
   const birthdayValue = birthdayEl.value
-  console.log(birthdayValue)
+  if (birthdayValue === "") {
+    alert("Please enter your birthday")
+  }
 }
 
 btnEl.addEventListener('click', calculateAge)
